ab11d16753f2c2a6b05e4627d6f4a411365a7355
hyewonSim <shim000113@naver.com>
2020-12-16 04:09:47 +0900

    Django Project

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,12 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post':post})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
@@ -39,4 +39,10 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
 
-    return render(request, 'blog/post_edit.html', {'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+
+    return redirect('blog:post_list')
--- bookmark/views.py
+++ bookmark/views.py
@@ -27,3 +27,4 @@ class BookmarkUpdateView(UpdateView):
 class BookmarkDeleteView(DeleteView):
     model = Bookmark
     success_url = reverse_lazy('bookmark:list')
+
